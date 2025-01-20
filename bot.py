ac1ba0ba591481e988eaf31aab1ad615f7bf91d0
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-21 02:18:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -234,5 +234,5 @@ async def handle_message(event: NewMessage.Event):
     await client.run_until_disconnected()
 
 if __name__ == "__main__":
- client.start()
-    client.run_until_disconnected()  # Эта строка будет запускать клиента и слушать сообщения
+client.start()
+client.run_until_disconnected()  # Эта строка будет запускать клиента и слушать сообщения
