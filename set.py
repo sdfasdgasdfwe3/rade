78aadab9062dd6e955a68348c0c76d01e0002ed9
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-21 06:54:31 +0500

    Update set.py

--- set.py
+++ set.py
@@ -1,1 +1,9 @@
+from telethon import TelegramClient
 
+# Функция, которая будет вызываться из bot.py
+async def magic_script(client: TelegramClient, event):
+    # Здесь вы используете уже авторизованный клиент
+    await event.respond("Скрипт magic был выполнен из set.py!")
+    # Можно продолжить с использованием клиента для любых действий
+    # Например, отправить сообщение
+    await client.send_message(event.chat_id, "Выполнил команду magic!")
