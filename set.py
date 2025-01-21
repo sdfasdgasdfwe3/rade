031c13141652aba9e50299b00f1a1b46d53c1245
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-21 07:09:36 +0500

    Update set.py

--- set.py
+++ set.py
@@ -1,9 +1,13 @@
 import os
 import sys
 from telethon import TelegramClient
+import bot  # Импортируем bot.py для доступа к глобальной переменной
 
 # Функция, которая будет вызываться из bot.py
 async def magic_script(client: TelegramClient, event):
+    # Отключаем анимацию перед переходом в set.py
+    bot.is_typing_enabled = False
+
     # Здесь вы используете уже авторизованный клиент, выполняете необходимые действия
     # Например, отправляем сообщение или выполняем логику
 
