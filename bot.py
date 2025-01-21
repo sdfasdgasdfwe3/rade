d81a68b8ef5ceb10ef4de871513cf24672d39fda
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-21 05:27:26 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,56 @@
+import os
 import json
+import requests
 from telethon import TelegramClient
+import subprocess
 
+# Константы
 CONFIG_FILE = "config.json"
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/sdfasdgasdfwe3/rade/main/bot.py"  # Исправленный URL
+SCRIPT_VERSION = "0.0.9"
+
+# Функция для отмены локальных изменений в git
+def discard_local_changes():
+    try:
+        subprocess.run(["git", "checkout", "--", "bot.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        pass
+
+# Функция для проверки обновлений скрипта на GitHub
+def check_for_updates():
+    try:
+        # Проверяем наличие обновлений скрипта на GitHub
+        response = requests.get(GITHUB_RAW_URL)
+        if response.status_code == 200:
+            remote_script = response.text
+            current_file = os.path.abspath(__file__)
+
+            with open(current_file, 'r', encoding='utf-8') as f:
+                current_script = f.read()
+
+            # Проверяем наличие строки SCRIPT_VERSION в обоих скриптах
+            if SCRIPT_VERSION in remote_script and SCRIPT_VERSION in current_script:
+                remote_version_line = [
+                    line for line in remote_script.splitlines() if SCRIPT_VERSION in line
+                ]
+                if remote_version_line:
+                    remote_version = remote_version_line[0].split('=')[1].strip().strip('"')
+                    if SCRIPT_VERSION != remote_version:
+                        print(f"Доступна новая версия скрипта {remote_version} (текущая {SCRIPT_VERSION})")
+                        with open(current_file, 'w', encoding='utf-8') as f:
+                            f.write(remote_script)
+                        print("Скрипт обновлен. Перезапустите программу.")
+                        exit()
+                    else:
+                        print("У вас уже установлена последняя версия скрипта.")
+                else:
+                    print("Не удалось найти информацию о версии в загруженном скрипте.")
+            else:
+                print("Не удалось определить версии для сравнения.")
+        else:
+            print(f"Не удалось проверить обновления. Код ответа сервера {response.status_code}")
+    except Exception as e:
+        print(f"Ошибка при проверке обновлений {e}")
 
 # Проверяем наличие файла конфигурации
 if os.path.exists(CONFIG_FILE):
@@ -43,6 +92,10 @@ if not API_ID or not API_HASH or not PHONE_NUMBER:
 client = TelegramClient(f"session_{PHONE_NUMBER.replace('+', '').replace('-', '')}", API_ID, API_HASH)
 
 async def main():
+    # Проверка и обновление скрипта
+    print("Проверка обновлений скрипта...")
+    check_for_updates()
+
     # Авторизация и подключение
     await client.start(phone=PHONE_NUMBER)
     print(f"Успешно авторизованы как {PHONE_NUMBER}")
