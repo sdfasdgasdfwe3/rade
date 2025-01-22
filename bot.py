add6a83110d889f97b682e4072deb7330bcb0d49
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-23 04:58:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ DEFAULT_TYPING_SPEED = 0.1
 SCRIPT_VERSION = "1.0.0"
 
 # GitHub URL для загрузки последней версии bot.py
-GITHUB_RAW_URL = "https://raw.githubusercontent.com/yourusername/yourrepo/main/bot.py"  # Обновите URL
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/sdfasdgasdfwe3/rade/main/bot.py"  # Обновите URL
 
 # Проверяем наличие файла конфигурации
 if os.path.exists(CONFIG_FILE):
