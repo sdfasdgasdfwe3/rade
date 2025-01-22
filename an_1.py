56627e1aabda55c00eac90ccccbe1e9863ca52f6
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-22 21:50:19 +0500

    Update an_1.py

--- an_1.py
+++ an_1.py
@@ -1,1 +1,13 @@
+import asyncio
 
+async def run_animation(client, event, text):
+    """Стандартная анимация текста"""
+    typing_speed = 1.5  # скорость печатания
+    cursor_symbol = "▮"  # Символ курсора
+
+    displayed_text = ""
+    for char in text:
+        displayed_text += char
+        await client.edit_message(event.chat_id, event.message.id, displayed_text + cursor_symbol)
+        await asyncio.sleep(typing_speed)
+    await client.edit_message(event.chat_id, event.message.id, displayed_text)
