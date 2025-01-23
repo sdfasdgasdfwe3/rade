7a0f2e218650f8902cd0ab5d1ce749174a32b4b2
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-01-23 11:23:44 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+api_id = 'YOUR_API_ID'  # Замените на ваш API ID
+api_hash = 'YOUR_API_HASH'  # Замените на ваш API Hash
+phone_number = 'YOUR_PHONE_NUMBER'  # Замените на ваш номер телефона
+repo_path = '/path/to/your/repo'  # Путь к репозиторию бота (если нужно обновление через Git)
