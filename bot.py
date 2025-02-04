e6830f0d2a2fbcba92e7fa458adf7fbb5019cdd5
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-02-05 03:58:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telethon import TelegramClient, events
 from telethon.errors import SessionPasswordNeededError, PhoneCodeInvalidError
 from configparser import ConfigParser
 
-VERSION = "1.9"
+VERSION = "2.0"
 GITHUB_RAW_URL = "https://raw.githubusercontent.com/sdfasdgasdfwe3/rade/main/bot.py"
 CONFIG_FILE = 'config.ini'
 SESSION_FILE = 'session_name'
