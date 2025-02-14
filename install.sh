eebbe690fc8d5fbfd27039d8280ea8eac8b63709
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-02-15 03:20:39 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,10 +59,23 @@ echo "-----------------------------------------"
 echo "Делаем главный файл исполняемым..."
 chmod +x bot.py
 
+# Проверка и создание tmux сессии с ботом
+echo "-----------------------------------------"
+echo "Проверка наличия активной tmux сессии..."
+tmux has-session -t session_name 2>/dev/null
+
+if [ $? != 0 ]; then
+    echo "Сессия не найдена, создаем новую сессию..."
+    tmux new-session -d -s session_name "python3 bot.py"
+else
+    echo "Подключаемся к существующей сессии..."
+    tmux attach -t session_name
+fi
+
+# Создаем .bashrc и добавляем автозапуск
 echo "-----------------------------------------"
 echo "Создаем .bashrc, если его нет, и добавляем автозапуск..."
 touch ~/.bashrc
-echo 'cd ~/rade && git pull && python3 bot.py' >> ~/.bashrc
+echo 'tmux has-session -t session_name 2>/dev/null || tmux new-session -d -s session_name "python3 ~/rade/bot.py"' >> ~/.bashrc
 
 echo "Установка завершена. Перезапустите Termux, чтобы бот запускался автоматически."
-
