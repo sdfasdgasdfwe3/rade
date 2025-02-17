146d8b69a64a15c06fef1621a39cd24852ed2376
sdfasdgasdfwe3 <53574617+sdfasdgasdfwe3@users.noreply.github.com>
2025-02-18 01:40:24 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,24 +3,19 @@
 source venv/bin/activate
 cd ~/rade
 
-# Удалить старый PID-файл
-rm -f .bot_pid
+# Завершить предыдущий процесс, если есть
+if [ -f .bot_pid ]; then
+    old_pid=$(cat .bot_pid)
+    kill -9 $old_pid 2>/dev/null
+    rm .bot_pid
+fi
 
-# Запустить бота в фоне и записать PID
+# Запустить бота и сохранить PID
 python3 bot.py &
 echo $! > .bot_pid
 
-# Функция для остановки
-cleanup() {
-    echo "Остановка бота..."
-    kill -9 $(cat .bot_pid) 2>/dev/null
-    rm -f .bot_pid
-}
-
-# Перехват сигналов
-trap cleanup EXIT TERM INT
+# Остановить бота при выходе из Termux
+trap 'kill -9 $(cat .bot_pid); rm .bot_pid' EXIT
 
 # Бесконечное ожидание
-while true; do
-    sleep 1
-done
+wait
